430003725d23163bf55023f529c15c8cd2139724
gokartgo <gokart.007@hotmail.com>
2023-03-10 11:28:37 +0700

    add auto pairs plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,9 @@
--- vim.cmd('source ~/.dotfiles/.config/nvim/vim-plug.vim')
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/vim-plug.lua")
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/coc.lua")
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/nvim-tree-conf.lua")
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/wilder.lua")
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/ctrlpvim.lua")
+dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/nvim-autopairs.lua")
 
 vim.opt.termguicolors = true
 vim.cmd('colorscheme OceanicNext')
--- .config/nvim/vim-plug.lua
+++ .config/nvim/vim-plug.lua
@@ -2,16 +2,18 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', vim.fn.stdpath('data') .. '/plugged')
 
-Plug 'mhartington/oceanic-next'
+Plug 'mhartington/oceanic-next' -- theme
 Plug('neoclide/coc.nvim', { branch = 'release' })
 Plug 'nvim-tree/nvim-web-devicons' -- optional, for file icons
-Plug 'nvim-tree/nvim-tree.lua'
+Plug 'nvim-tree/nvim-tree.lua' -- file explorer
 Plug 'ctrlpvim/ctrlp.vim' -- Full path fuzzy file, buffer, mru, tag, ... finder for Vim.
+Plug 'windwp/nvim-autopairs' -- auto pairs ) } ]
 
 -- auto complete in Ex commands
   function UpdateRemotePlugins(...)
     -- Needed to refresh runtime files
     vim.cmd('let &rtp=&rtp')
+    -- Run :UpdateRemotePlugins
     vim.cmd('UpdateRemotePlugins')
   end
 Plug('gelguy/wilder.nvim', { ['do'] = vim.fn['UpdateRemotePlugins'] })

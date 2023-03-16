427c54c20d609f8ef9774406703a0d9cf227e137
gokartgo <gokart.007@hotmail.com>
2023-03-16 17:26:31 +0700

    move vim option from init.lua to lua/user/options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,10 @@
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/vim-plug.lua")
-dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/coc.lua")
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/nvim-tree-conf.lua")
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/wilder.lua")
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/ctrlpvim.lua")
+-- add before coc because problem key map <CR>
 dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/nvim-autopairs.lua")
+dofile(os.getenv("HOME") .. "/.dotfiles/.config/nvim/coc.lua")
 
-vim.opt.termguicolors = true
-vim.cmd('colorscheme OceanicNext')
-
--- set line number
-vim.opt.number = true
--- set copy to clipboard
-vim.opt.clipboard = 'unnamedplus'
--- set ignore case
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
--- set tab indent = 2
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.bo.softtabstop = 2
-
+require "user.options"
+require "user.keymaps"

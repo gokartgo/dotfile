7321ad5f3d0f2accb0160ec2fc2752f040eb716a
gokartgo <gokart.007@hotmail.com>
2023-09-10 14:24:26 +0700

    UPDATE: add keymapping

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -5,12 +5,19 @@ local keymap = vim.api.nvim_set_keymap
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
 -- Normal
 -- Better Window Navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Split Window
+keymap("n", "<leader>h", "<C-w>v", opts) -- Split window horizontal
+keymap("n", "<leader>v", "<C-w>s", opts) -- Split window vertical
+keymap("n", "<leader>q", "<C-w>q", opts) -- Quit current window
+
 -- Toggle Explore from NvimTree Plugin
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
@@ -23,3 +30,10 @@ keymap("n", "<S-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate Buffer
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
+
+-- Move text up and down and auto adjust indent
+keymap("v", "<S-k>", ":move-2<CR>gv=gv", opts)
+keymap("v", "<S-j>", ":move'>+<CR>gv=gv", opts)
+
+-- Replace Without Copy Deleted Word
+keymap("v", "p", '"_dP', opts)

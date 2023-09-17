6b476a8dd653339d14e005e387aed50e4139d9db
gokartgo <gokart.007@hotmail.com>
2023-09-17 14:43:54 +0700

    UPDATE: add config to wilder to ignore case sensitive

--- .config/nvim/wilder.lua
+++ .config/nvim/wilder.lua
@@ -5,7 +5,8 @@ wilder.setup({
   next_key = '<Tab>',
   previous_key = '<S-Tab>',
   accept_key = '<Down>',
-  reject_key = '<Up>'
+  reject_key = '<Up>',
+  case_sensitive = 0
 })
 
 -- 'highlighter' : applies highlighting to the candidates

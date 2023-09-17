a33276a23a899f39b34ece18da9450faded5947a
gokartgo <gokart.007@hotmail.com>
2023-09-17 14:44:14 +0700

    UPDATE: map key for nvim tree plugin

--- .config/nvim/nvim-tree-conf.lua
+++ .config/nvim/nvim-tree-conf.lua
@@ -5,8 +5,19 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
--- require("nvim-tree").setup()
+local function custom_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- use all default mappings 
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- override default mappings
+  vim.keymap.set('n', '<C-i>', api.node.show_info_popup, opts('Info'))
+end
 
 -- OR setup with some options
 require("nvim-tree").setup({
@@ -14,6 +25,7 @@ require("nvim-tree").setup({
   view = {
     width = 30,
   },
+  on_attach = custom_on_attach,
   renderer = {
     group_empty = true,
   },

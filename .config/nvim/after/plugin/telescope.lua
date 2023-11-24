31ec75925bb54ac2cd1f11be69016ea8d7dcea26
gokartgo <gokart.007@hotmail.com>
2023-11-24 12:08:13 +0700

    UPDATE: add keymap for find text in all file

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -6,7 +6,12 @@ if not builtin_status_ok then
 end
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader><S-f>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
-  builtin.grep_string({ search = vim.fn.input("grep string > ") })
+  builtin.grep_string({ search = vim.fn.input("Find Text: ") })
 end)
+vim.keymap.set('v', '<leader>F', function()
+  local text = vim.g.get_visual_selection()
+  builtin.grep_string({ search = text })
+end)
+

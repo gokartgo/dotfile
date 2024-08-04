d2d05d0ac4067e39bd161b456bb9970eadff5d4b
Kritchanon Limtongsittikun <kritchanon@Kritchanons-MacBook-Pro.local>
2024-08-04 12:29:49 +0700

    UPDATE: create manual backspace for nvim-autopairs to avoid conflict from vim-multiple-select

--- .config/nvim/lua/plugin/nvim-autopairs.lua
+++ .config/nvim/lua/plugin/nvim-autopairs.lua
@@ -5,4 +5,24 @@ if not nvim_autopairs_status_ok then
   return
 end
 
-nvim_autopairs.setup {}
+nvim_autopairs.setup {
+  map_bs = false
+}
+
+_G.MUtils = {
+  BS = function()
+    if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
+      return nvim_autopairs.esc('<c-e>') .. nvim_autopairs.autopairs_bs()
+    else
+      return nvim_autopairs.autopairs_bs()
+    end
+  end
+
+}
+
+-- The reason why we cannot use vim.keymap.set is because autopairs_bs() returns
+-- the string ^GU<80>kb<80>kD, which is converted from <C-g>U<BS><Del> by
+-- the function vim.api.nvim_replace_termcodes. This function returns a termcodes string,
+-- which vim.keymap.set cannot interpret.
+vim.api.nvim_set_keymap('i', '<BS>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
+

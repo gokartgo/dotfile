2831cd33effe4422d06fcd83efdd3a929d3bc28f
Kritchanon Limtongsittikun <kritchanon@Kritchanons-MacBook-Pro.local>
2024-08-05 11:52:21 +0700

    UPDATE: improve yamlls to support docker-compose

--- .config/nvim/lua/plugin/lsp/settings/yamlls.lua
+++ .config/nvim/lua/plugin/lsp/settings/yamlls.lua
@@ -3,8 +3,20 @@ return {
     yaml = {
       filetypes = { "yml", "yaml", "yaml.docker-compose" },
       schemas = {
-        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        ["https://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+        ["https://json.schemastore.org/composer"] = "docker-compose.yaml",
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose*.{yml,yaml}",
       },
+      schemaStore = {
+        -- You can enable the schema store feature if needed
+        enable = true
+      },
+      format = {
+        enable = true,
+      },
+      validate = true,
+      hover = true,
+      completion = true,
     },
   },
 }

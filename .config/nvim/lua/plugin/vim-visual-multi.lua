e1a87773d3731abffd9e6660bf3b5d7abeb3fc01
Kritchanon Limtongsittikun <kritchanon@Kritchanons-MacBook-Pro.local>
2025-03-23 17:34:38 +0700

    UPDATE: update config to vim-visual-multi

--- .config/nvim/lua/plugin/vim-visual-multi.lua
+++ .config/nvim/lua/plugin/vim-visual-multi.lua
@@ -1,8 +1,25 @@
+vim.g.VM_default_mappings = 0
+vim.g.VM_mouse_mappings = 0
+vim.g.VM_add_cursor_at_pos_no_mappings = 1
+vim.g.VM_highlight_matches = 'underline'
+
+vim.g.VM_leader = ','
+
+local leader = vim.g.VM_leader
+
 vim.g.VM_maps = {
-  ['Find Next'] = 'n',
-  ['Find Prev'] = 'N',
-  ['Increase'] = '<C-y>',
-  ['Decrease'] = '<C-x>',
-  ['gIncrease'] = 'g<C-y>',
-  ['gDecrease'] = 'g<C-x>',
+  ['Add Cursor At Pos'] = leader .. ',',
+  ['Add Cursor Down']   = leader .. 'j',
+  ['Add Cursor Up']     = leader .. 'k',
+  ['Find Next']         = 'n',
+  ['Find Prev']         = 'N',
+  ['Seek Up']           = '', -- go to previous page in same file for the file bigger than 1 screen
+  ['Seek Down']         = '', -- go to next page in same file for the file bigger than 1 screen
+  ['Increase']          = '<C-y>',
+  ['Decrease']          = '<C-x>',
+  ['gIncrease']         = 'g<C-y>',
+  ['gDecrease']         = 'g<C-x>',
+  ['Select l']          = '',
+  ['Select h']          = '',
+  ['Toggle Mappings']   = leader .. '<space>',
 }
